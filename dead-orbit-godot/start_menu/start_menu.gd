9f020056429a39c3500c345cf1e34bc2c3509c8d
Zoya Soy <zoyasoy01@gmail.com>
2026-05-31 15:31:06 -0700

    ..

--- dead-orbit-godot/start_menu/start_menu.gd
+++ dead-orbit-godot/start_menu/start_menu.gd
@@ -6,12 +6,19 @@ func _ready():
 	$StartButton.pressed.connect(_on_start_pressed)
 	$CreditsButton.pressed.connect(_on_credits_pressed)
 	$CreditsPanel/BackButton.pressed.connect(_on_credits_back)
-	FadeManager.fade_in()
+	FadeManager.fade_in(0.3)
 	_update_button_highlight()
+	await get_tree().create_timer(0.5).timeout
+	await get_tree().process_frame
+	_input_enabled = true
 
 var _joy_moved := false
+var _input_enabled := false
 
 func _process(_delta):
+	if not _input_enabled:
+		return
+
 	var axis := Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
 
 	if axis > 0.5 and not _joy_moved:
@@ -38,7 +45,7 @@ func _update_button_highlight():
 	$CreditsButton.modulate = Color(1.0, 1.0, 1.0) if selected_button == 1 else Color(0.5, 0.5, 0.5)
 
 func _on_start_pressed():
-	FadeManager.fade_to_scene("res://start_menu/character_select.tscn")
+	FadeManager.fade_to_scene("res://start_menu/character_select.tscn", 0.1)
 
 func _on_credits_pressed():
 	$StartButton.visible = false
